c9ec44e11968fe6b699a7f310f70dfe2aa32acd4
Caner Ertano <canerertano@gmail.com>
2017-10-28 22:21:14 +0300

    design changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var myIndex = 0;
 carousel();
 
+var description = "I’m a self-motivated and passionate software developer with 3+ years of industry experience. I’m currently in the J2EE world as a full time worker. Alongside of working time I spend my remaining time with Python and web development. I always have interest to learning new technologies and tools. Also as you can see in this web page I spend my other time with photography :)";
+
+document.getElementById("desc-text").innerHTML  = description;
+document.getElementById("desc-text2").innerHTML  = description;
+
 function carousel() {
 	var x = document.getElementsByClassName('carouselImage');
 
