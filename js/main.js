25b4b558c169ded2cc681d59be7ae8eb59f1e267
Caner Ertano <canerertano@gmail.com>
2018-04-05 20:53:24 +0300

    update main.js
    Description updated.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var myIndex = 0;
 carousel();
 
-var description = "I’m a self-motivated and passionate software developer with 3+ years of industry experience. I’m currently in the J2EE world as a full time worker. Alongside of working time I spend my remaining time with Python and web development. I always have interest to learning new technologies and tools. Also as you can see in this web page I spend my other time with photography :)";
+var description = "I’m a self-motivated and passionate software engineer with almost 4 years of industry experience. One of my goals is to became a Software Architect in this profession and other is contribute as much as I can to open-source community. To achieve my goals I work hard at both work and home. Along with that I spend my remaining time with Java, Python, Javascript, React.js and web development. Also I have always interest to learning new technologies and tools. Also as you can see in this web page I spend my other time with photography :)";
 
 document.getElementById("desc-text").innerHTML  = description;
 document.getElementById("desc-text2").innerHTML  = description;
